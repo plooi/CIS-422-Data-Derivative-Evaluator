3a0ae34abab81d022b9fe3106c8c68b47c63efb3
Brian Truong <truonghbrian@gmail.com>
2019-05-30 17:43:37 -0700

    plot function implemented in plot_display

--- gui/plot_display.py
+++ gui/plot_display.py
@@ -27,7 +27,20 @@ class PlotDisplay(Frame):
         # a = f.add_subplot(111)
         # a.plot([1, 2, 3, 4, 5, 6, 7, 8], [5, 6, 1, 3, 8, 9, 3, 5])
         #gcm, mdm, rcp, hms, max
-        f = plot.plot_button('HadGEM2-CC', 'bcsd', 'RCP85', 'PRMS', 1)
+        # f = plot.plot_button('HadGEM2-CC', 'bcsd', 'RCP85', 'PRMS', 1)
+
+        # canvas = FigureCanvasTkAgg(f, self)
+        # canvas.draw()
+        # canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=True)
+        # # canvas.get_tk_widget().grid(stick=S)
+
+        # toolbar = NavigationToolbar2Tk(canvas, self)
+        # toolbar.update()
+        # canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
+        # canvas._tkcanvas.grid(sticky=S, expand=True)
+    def plot(self, f: Figure):
+        for child in self.winfo_children():
+            child.destroy()
 
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
@@ -37,4 +50,4 @@ class PlotDisplay(Frame):
         toolbar = NavigationToolbar2Tk(canvas, self)
         toolbar.update()
         canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=True)
-        # canvas._tkcanvas.grid(sticky=S, expand=True)
+        canvas._tkcanvas.grid(sticky='s', expand=True)
--- gui/projections_list.py
+++ gui/projections_list.py
@@ -16,3 +16,30 @@ class ProjectionsList(Frame):
         locationlist.grid(sticky='ns')
         locationlist.insert(END, "Test location 1")
         locationlist.insert(END, "Test location 2")
+
+        # # creating a scrollbars
+        # self.xscrlbr = Scrollbar(self, orient = 'horizontal')
+        # self.xscrlbr.grid(column = 0, row = 1, sticky = 'ew', columnspan = 2)
+        # self.yscrlbr = Scrollbar(self)
+        # self.yscrlbr.grid(column = 1, row = 0, sticky = 'ns')
+        # # creating a canvas
+        # self.canv = Canvas(self)
+        # self.canv.config(relief = 'flat', width = canv_w, bd = 2)
+        # # placing a canvas into frame
+        # self.canv.grid(column = 0, row = 0, sticky = 'nsew')
+        # # accociating scrollbar comands to canvas scroling
+        # self.xscrlbr.config(command = self.canv.xview)
+        # self.yscrlbr.config(command = self.canv.yview)
+
+        # # creating a frame to inserto to canvas
+        # self.scrollwindow = Frame(self)
+
+        # self.canv.create_window(0, 0, window = self.scrollwindow, anchor = 'nw')
+
+        # self.canv.config(xscrollcommand = self.xscrlbr.set,
+        #                  yscrollcommand = self.yscrlbr.set,
+        #                  scrollregion = (0, 0, 100, 100))
+
+        # self.yscrlbr.lift(self.scrollwindow)
+        # self.xscrlbr.lift(self.scrollwindow)
+        # self.scrollwindow.bind('<Configure>', self._configure_window)
