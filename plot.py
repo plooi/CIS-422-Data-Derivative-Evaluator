538a1325d5afff48e438c0cf8626ce763e59b5ba
Laura Queen <lauraqueen@Lauras-MacBook-Pro-5.local>
2019-06-01 14:35:06 -0700

    got rid of repeated calculations

--- plot.py
+++ plot.py
@@ -15,36 +15,17 @@ data_array: an xarray dataarray object with streamflow values for a certain proj
 max: a Boolean value. True: plot the maximum streamflow, False: plot the mean streamflow.
 '''
 
-
-def plot_timeseries(projections):
-
-    figure = Figure(figsize=(8,5), dpi=100)
-    if projection.max:
-        figure.title('Annual Maximum Daily Streamflow')
-    else:
-        figure.title('Annual Mean Daily Streamflow')
-    for projection in projections:
-        # label = projection.rcp + "_" + projection.gcm + "_" + projection.mdm + "_" + projection.hms
-        newPlot = figure.add_subplot(1, 1, 1)
-
-        values = []
-        for i in range(2100 - 1950):
-            year = str(1950 + i)
-            if projection.max:
-                val = np.max(projection.data.sel(time=year))
-            if not projection.max:
-                val = np.mean(projection.data.sel(time=year))
-            values.append(val)
-
-        newPlot.set_ylabel('cfs')
-        newPlot.set_xlabel('year')
-        newPlot.plot([1950+i for i in range(150)], values, color='blue')
-        newPlot.show()
-    return figure
-
 def add_projection(gcm: str, mdm: str, rcp: str, hms: str, color: str, mx: bool):
     data = all_data.sel(parameters=hms, downscale_method=mdm, gcm=gcm, rcp=rcp)
-    new_projection = Projection(rcp, gcm, mdm, hms, color, data, mx)
+    annual_values = []
+    for i in range(2100 - 1950):
+        year = str(1950 + i)
+        if mx:
+            val = np.max(data.sel(time=year))
+        if not mx:
+            val = np.mean(data.sel(time=year))
+        annual_values.append(val)
+    new_projection = Projection(rcp, gcm, mdm, hms, color, annual_values, mx)
     active_projections.append(new_projection)
 
 def create_figure() -> Figure:
@@ -57,16 +38,6 @@ def create_figure() -> Figure:
     for projection in active_projections:
         if not (projection.get_visibility()):
             continue
-        # label = projection.rcp + "_" + projection.gcm + "_" + projection.mdm + "_" + projection.hms
-        values = []
-        for i in range(2100 - 1950):
-            year = str(1950 + i)
-            if projection.max:
-                val = np.max(projection.data.sel(time=year))
-            if not projection.max:
-                val = np.mean(projection.data.sel(time=year))
-            values.append(val)
-
-        newPlot.plot([1950+i for i in range(150)], values, projection.get_color())
+        newPlot.plot([1950+i for i in range(150)], projection.data, projection.get_color())
 
     return figure
